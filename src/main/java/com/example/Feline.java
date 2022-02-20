2d4629da404da1da1341f498663d4ce882da07ab
Daniil <sampir_danik@mail.ru>
2022-02-20 14:49:01 +0300

    First commit

---
+++ src/main/java/com/example/Feline.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.List;
+
+public class Feline extends Animal implements Predator, IFeline {
+
+    @Override
+    public List<String> eatMeat() throws Exception {
+        return getFood("Хищник");
+    }
+
+    @Override
+    public List<String> getFood() throws Exception {
+        return getFood("Хищник");
+    }
+
+    @Override
+    public String getFamily() {
+        return "Кошачьи";
+    }
+
+    public int getKittens() {
+        return getKittens(1);
+    }
+
+    public int getKittens(int kittensCount) {
+        return kittensCount;
+    }
+}
